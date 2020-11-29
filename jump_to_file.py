6c8ba4bb21408a5680c763fcf217b6e02f65f0a6
Russell Davis <551404+russelldavis@users.noreply.github.com>
2020-11-29 13:44:25 -0800

    jump_to_file: open urls, try .js extension

--- jump_to_file.py
+++ jump_to_file.py
@@ -1,20 +1,88 @@
 import sublime
 import sublime_plugin
+import webbrowser
 import os
 from os.path import splitext
 
-def _try_open(window, try_file, path=None):
+def _try_open_file(window, try_file, path=None):
     if path:
         try_file = os.path.join(path, try_file)
 
-    if not os.path.isfile(try_file):
-        try_file += '.rb'
+    for ext in ['', '.rb', '.js']:
+        with_ext = try_file + ext
+        if os.path.isfile(with_ext):
+            window.open_file(with_ext)
+            return True
 
-    if os.path.isfile(try_file):
-        window.open_file(try_file)
-        return True
+    return False
+
+
+def try_open_region_file(view, region):
+    window = view.window()
+    # The scope includes the quote characters, so we slice them off
+    try_file = view.substr(view.extract_scope(region.begin()))[1:-1]
+
+    if os.path.isabs(try_file):
+        _try_open_file(window, try_file)
+        return
+
+    folders = window.folders()
+    if folders:
+        for folder in folders:
+            if _try_open_file(window, try_file, folder):
+                return
+
+    view_file = view.file_name()
+    if view_file:
+        view_dir = os.path.dirname(view_file)
+        if _try_open_file(window, try_file, view_dir):
+            return
+
+    msg = "Not a file: %s" % try_file
+    print(msg)
+    sublime.status_message(msg)
+
+
+# From https://gist.github.com/korinVR/3542836
+def try_open_region_url(view, region):
+    s = view.sel()[0]
+
+    # Expand selection to possible URL
+    start = s.a
+    end = s.b
+
+    view_size = view.size()
+    terminator = ['\t', ' ', '\"', '\'', '(', ')']
+
+    while (start > 0
+            and not view.substr(start - 1) in terminator
+            and view.classify(start) & sublime.CLASS_LINE_START == 0):
+        start -= 1
+
+    while (end < view_size
+            and not view.substr(end) in terminator
+            and view.classify(end) & sublime.CLASS_LINE_END == 0):
+        end += 1
+
+    # Check if this is URL
+    url = view.substr(sublime.Region(start, end))
+    print("URL : " + url)
+
+    if url.startswith(('http://', 'https://')):
+        webbrowser.open_new_tab(url)
     else:
-        return False
+        print("not URL")
+
+
+class JumpToFile(sublime_plugin.TextCommand):
+    def run(self, edit = None):
+        view = self.view
+        for region in view.sel():
+            if view.score_selector(region.begin(), "parameter.url, string.quoted"):
+                try_open_region_file(view, region)
+            else:
+                try_open_region_url(view, region)
+
 
 # Derp, this is already built into sublime (the switch_file command)
 #
@@ -38,34 +106,3 @@ def _try_open(window, try_file, path=None):
 #                 if file_ext == impl_ext:
 #                     if _try_open(window, base + ".h"):
 #                         return
-
-
-class JumpToFile(sublime_plugin.TextCommand):
-
-    def run(self, edit = None):
-        view = self.view
-        window = view.window()
-        for region in view.sel():
-            if view.score_selector(region.begin(), "parameter.url, string.quoted"):
-                # The scope includes the quote characters, so we slice them off
-                try_file = view.substr(view.extract_scope(region.begin()))[1:-1]
-
-                if os.path.isabs(try_file):
-                    _try_open(window, try_file)
-                    continue
-
-                folders = view.window().folders()
-                if folders:
-                    for folder in folders:
-                        if _try_open(window, try_file, folder):
-                            continue
-
-                view_file = view.file_name()
-                if view_file:
-                    view_dir = os.path.dirname(view_file)
-                    if _try_open(window, try_file, view_dir):
-                        continue
-
-                msg = "Not a file: %s" % try_file
-                print(msg)
-                sublime.status_message(msg)
